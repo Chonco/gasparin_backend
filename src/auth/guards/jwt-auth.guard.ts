9aff72655c0ac5077cbb79656b66cfee235f7789
Bruno Hernandez <bruno@bridgecode.dev>
2022-06-03 13:27:01 -0500

    refresh token strategy
    authorization refactorization

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -3,9 +3,10 @@ import { Reflector } from "@nestjs/core";
 import { AuthGuard } from "@nestjs/passport";
 import { Observable } from "rxjs";
 import { IS_PUBLIC_KEY } from '../../config/public.key';
+import { STRATEGY_JWT_AUTH } from '../constants/strategies.contants';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
+export class JwtAuthGuard extends AuthGuard(STRATEGY_JWT_AUTH) {
     constructor(private reflector: Reflector) {
         super();
     }
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,25 +1,26 @@
+import { STRATEGY_JWT_AUTH } from './../constants/strategies.contants';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { JwtPayload } from '../constants/jwt-constants';
-import { RequestUser } from '../constants/request-user';
+import { JwtPayload } from '../constants/jwt.constants';
 import { UserService } from '../../user/services/user.service';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { UserAccessTokenClaims } from '../dtos/user-token-claims.dto';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, STRATEGY_JWT_AUTH) {
     constructor(
         private userService: UserService,
         private configService: ConfigService
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
             secretOrKey: configService.get('jwt.secret'),
+            ignoreExpiration: false,
         });
     }
 
-    async validate(payload: JwtPayload): Promise<RequestUser> {
+    async validate(payload: JwtPayload): Promise<UserAccessTokenClaims> {
         const user = await this.userService.getById(payload.sub);
 
         return {
